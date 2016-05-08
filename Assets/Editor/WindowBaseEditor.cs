2096247c0b9376ebe31e90710f6fa09f1009c143
Goo <kuwoods@hotmail.com>
2016-05-08 08:24:02 +0800

    increase WindowBaseEditor for WindowBase.

---
+++ Assets/Editor/WindowBaseEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WindowBase), true)]
+public class WindowBaseEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        WindowBase windowBase = (WindowBase)target;
+        windowBase.InShowingType = (WindowManager.ShowingType)EditorGUILayout.EnumPopup("InShowingType:", windowBase.InShowingType);
+        switch (windowBase.InShowingType)
+        {
+            case WindowManager.ShowingType.Panning:
+                windowBase.InPosition = EditorGUILayout.Vector2Field("InPosition:", windowBase.InPosition);
+                break;
+        }
+        windowBase.OutShowingType = (WindowManager.ShowingType)EditorGUILayout.EnumPopup("OutShowingType:", windowBase.OutShowingType);
+        switch (windowBase.OutShowingType)
+        {
+            case WindowManager.ShowingType.Panning:
+                windowBase.OutPosition = EditorGUILayout.Vector2Field("OutPosition:", windowBase.OutPosition);
+                break;
+        }
+        windowBase.ShowingTime = EditorGUILayout.FloatField("ShowingTime:", windowBase.ShowingTime);
+        base.OnInspectorGUI();
+    }
+}
--- Assets/Scripts/WindowBase.cs
+++ Assets/Scripts/WindowBase.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class WindowBase : MonoBehaviour {
     #region setting at the object
+    [HideInInspector]
     public WindowManager.ShowingType InShowingType = WindowManager.ShowingType.Pop;
+    [HideInInspector]
     public Vector2 InPosition = Vector2.zero;
+    [HideInInspector]
     public WindowManager.ShowingType OutShowingType = WindowManager.ShowingType.Pop;
+    [HideInInspector]
     public Vector2 OutPosition = Vector2.zero;
+    [HideInInspector]
     public float ShowingTime = 0.2f;
     #endregion setting at the object
 }
